5c97986895b411360a738afbf3dd422de17b7070
Im3vulx <loic.hlppl@gmail.com>
2024-02-20 14:59:28 +0100

    📦 NEW:  Refactor code to use class and add CSV import/export functionality

--- src/app/calulette/calulette.component.ts
+++ src/app/calulette/calulette.component.ts
@@ -1,6 +1,12 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Calcul {
+  titre: string;
+  resultat: string;
+  unite: string;
+}
 
 @Component({
   selector: 'app-calulette',
@@ -10,20 +16,18 @@ import { FormsModule } from '@angular/forms';
     CommonModule
   ],
   templateUrl: './calulette.component.html',
-  styleUrl: './calulette.component.scss'
+  styleUrls: ['./calulette.component.scss']
 })
 export class CaluletteComponent {
-  calcule: string = '';
-  total: string = '';
-  produit: string = '';
-  unite: string = '';
-  listeCalculs: { titre: string, resultat: string, unite: string }[] = [];
+  calcule = '';
+  total = '';
+  produit = '';
+  unite = 'm²';
+  listeCalculs: Calcul[] = [];
 
   constructor() {
     const storedCalculs = localStorage.getItem('calculs');
-    if (storedCalculs) {
-      this.listeCalculs = JSON.parse(storedCalculs);
-    }
+    this.listeCalculs = storedCalculs ? JSON.parse(storedCalculs) : [];
   }
 
   onButtonClicked(buttonText: string) {
@@ -43,19 +47,111 @@ export class CaluletteComponent {
 
   ajouterALaListe() {
     if (this.produit && this.calcule && this.total && this.unite) {
-      this.listeCalculs.push({
-        titre: this.produit,
-        resultat: this.total,
-        unite: this.unite
-      });
+      const elementExist = this.listeCalculs.find(item => item.titre === this.produit);
+      if (elementExist) {
+        // L'élément existe déjà, nous le mettons à jour
+        this.updateElement(elementExist);
+      } else {
+        // L'élément n'existe pas encore, nous l'ajoutons à la liste
+        this.listeCalculs.push({ titre: this.produit, resultat: this.total, unite: this.unite });
+      }
 
-      localStorage.setItem('calculs', JSON.stringify(this.listeCalculs));
-
-      // Réinitialisation des champs après l'ajout à la liste
-      this.produit = '';
-      this.calcule = '';
-      this.total = '';
-      this.unite = ''; 
+      this.updateLocalStorageAndResetFields();
     }
   }
+
+  selectionnerElement(calcul: Calcul) {
+    this.produit = calcul.titre;
+    this.calcule = calcul.resultat;
+    this.unite = calcul.unite;
+  }
+
+  private updateElement(existingElement: Calcul) {
+    existingElement.resultat = this.total;
+    existingElement.unite = this.unite;
+  }
+
+  private updateLocalStorageAndResetFields() {
+    localStorage.setItem('calculs', JSON.stringify(this.listeCalculs));
+    this.resetFields();
+  }
+
+  private resetFields() {
+    this.produit = '';
+    this.calcule = '';
+    this.total = '';
+    this.unite = 'm²';
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.parseCSV(file);
+    }
+  }
+
+  parseCSV(file: File) {
+    const reader = new FileReader();
+    reader.readAsText(file);
+
+    reader.onload = () => {
+      const csvData: string = reader.result as string;
+      const lines: string[] = csvData.split('\n');
+      
+      lines.shift();
+
+      lines.forEach((line, index) => {
+        const columns: string[] = line.split(',');
+        if (columns.length === 3) {
+          const titre: string = columns[0];
+          const resultat: string = columns[1];
+          const unite: string = columns[2];
+          this.listeCalculs.push({ titre, resultat, unite });
+        } else {
+          alert(`La ligne ${index + 1} est mal formatée.`);
+        }
+      });
+      this.updateLocalStorageAndResetFields();
+    };
+
+    reader.onerror = () => {
+      console.error('Erreur de lecture du fichier.');
+    };
+  }
+
+  exportToCSV() {
+  // Nom des colonnes dans le fichier CSV
+  const csvHeader = ['Titre', 'Résultat', 'Unité'];
+
+  // Contenu des lignes du fichier CSV
+  const csvData = this.listeCalculs.map(calcul => [calcul.titre, calcul.resultat, calcul.unite].join(','));
+
+  // Contenu final du fichier CSV
+  const csvContent = [csvHeader.join(','), ...csvData].join('\n');
+
+  // Création d'un objet Blob pour télécharger le fichier CSV
+  const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+
+  // Création d'un objet URL à partir du Blob
+  const url = URL.createObjectURL(blob);
+
+  // Création d'un élément d'ancrage invisible pour le téléchargement
+  const a = document.createElement('a');
+  a.setAttribute('style', 'display:none');
+  document.body.appendChild(a);
+
+  // Attribution de l'URL au lien d'ancrage
+  a.href = url;
+
+  // Spécification du nom du fichier CSV à télécharger
+  a.download = 'liste_calculs.csv';
+
+  // Clic sur le lien d'ancrage pour déclencher le téléchargement
+  a.click();
+
+  // Nettoyage
+  window.URL.revokeObjectURL(url);
+  document.body.removeChild(a);
+}
+
 }
